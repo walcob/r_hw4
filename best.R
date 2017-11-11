a2d71c3cc8ee4f7b6367c10bad0bc3be9a80cad8
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-11-11 16:57:11 -0500

    Rewrote best.R to implement rankhospital.R

--- best.R
+++ best.R
@@ -1,46 +1,54 @@
-best <- function(state,outcome) {
-    ## Read outcome data
-    outcomeData <- read.csv("outcome-of-care-measures.csv",colClasses="character")
-    availableOutcomes <- c(11,17,23)
-    names(availableOutcomes) <- c("heart attack","heart failure","pneumonia")
-    ## 11, 17, 24
-    ## Check that state and outcome are valid
-    if(any(outcomeData$State == state)){
-        if(any(names(availableOutcomes) == outcome)){
-            ## Extract data for just the state
-            stateData <- outcomeData[outcomeData$State==state,]
-            ## Extract data for just the outcome given and the name of the hospital
-            nameAndDeathRate <- data.frame(stateData[2],stateData[availableOutcomes[outcome]])
-            ## Rename second column to make it easier to use
-            names(nameAndDeathRate)[2] <- "deathrate"
-            ## Make the death rates numeric, suppress the warnings about
-            ## introducing NAs
-            nameAndDeathRate[2] <- suppressWarnings(lapply(nameAndDeathRate[2],as.numeric))
-            ## Remove NAs
-            nameAndDeathRate <- nameAndDeathRate[complete.cases(nameAndDeathRate),]
-            ## Get the minimum rate
-            minRate <- sapply(nameAndDeathRate[2],min)
-            ## Check for multiple hospitals with this rate and return the first in alphabetical order
-            minima <- nameAndDeathRate[nameAndDeathRate$deathrate==minRate,]
-            result <- sapply(minima[1],min)
-            names(result) <- NULL
-            return(result)
-        }
-        else{
-            stop("invalid outcome")
-        }
-    }
-    else{
-        stop("invalid state")
-    }
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
-}
+source("rankhospital.R")
 
-plotMortality <- function(){
-    outcome <- read.csv("outcome-of-care-measures.csv",colClasses="character")
-    head(outcome)
-    outcome[,11] <- as.numeric(outcome[,11])
-    hist(outcome[,11])
-    
+best <- function(state,outcome) {
+    ## Oh hey, we can reimplement this to call rankhospital with 
+    ## num = 1.  
+    return(rankhospital(state,outcome,1))
 }
+#     ## Read outcome data
+#     outcomeData <- read.csv("outcome-of-care-measures.csv",colClasses="character")
+#     availableOutcomes <- c(11,17,23)
+#     names(availableOutcomes) <- c("heart attack","heart failure","pneumonia")
+#     ## Check that state and outcome are valid
+#     if(any(outcomeData$State == state)){
+#         if(any(names(availableOutcomes) == outcome)){
+#             ## Extract data for just the state
+#             stateData <- outcomeData[outcomeData$State==state,]
+#             ## Extract data for just the outcome given and the name of the hospital
+#             nameAndDeathRate <- data.frame(stateData[2],stateData[availableOutcomes[outcome]])
+#             ## Rename second column to make it easier to use
+#             names(nameAndDeathRate)[2] <- "deathrate"
+#             ## Make the death rates numeric, suppress the warnings about
+#             ## introducing NAs
+#             nameAndDeathRate[2] <- suppressWarnings(lapply(nameAndDeathRate[2],as.numeric))
+#             ## Remove NAs
+#             nameAndDeathRate <- nameAndDeathRate[complete.cases(nameAndDeathRate),]
+#             ## Get the minimum rate
+#             minIndex <- order()
+# 
+#             minRate <- sapply(nameAndDeathRate[2],min)
+#             ## Check for multiple hospitals with this rate and return the first in alphabetical order
+#             minima <- nameAndDeathRate[nameAndDeathRate$deathrate==minRate,]
+#             result <- sapply(minima[1],min)
+#             names(result) <- NULL
+# 
+#             ## Return hospital name in that state with lowest 30-day death
+#             ## rate 
+#             return(result)
+#         }
+#         else{
+#             stop("invalid outcome")
+#         }
+#     }
+#     else{
+#         stop("invalid state")
+#     }
+# }
+# 
+# plotMortality <- function(){
+#     outcome <- read.csv("outcome-of-care-measures.csv",colClasses="character")
+#     head(outcome)
+#     outcome[,11] <- as.numeric(outcome[,11])
+#     hist(outcome[,11])
+# 
+# }
